efabba036cbcb6a8cfa92a5041e551128d891902
furti <daniel.furtlehner@gmx.net>
2017-10-24 14:48:47 +0200

    adds script to import users and create passwords

--- database/scripts/furti.ts
+++ database/scripts/furti.ts
@@ -1,80 +1,73 @@
 import { MongoClient, Db, Cursor } from 'mongodb';
 
-export class Furti
-{
+export class Furti {
     private password: string;
     private username: string;
     private db: Db;
 
-    constructor(username: string, password: string)
-    {
+    constructor(username: string, password: string) {
         this.username = encodeURIComponent(username);
         this.password = encodeURIComponent(password);
     }
 
-    connect(): Promise<Furti>
-    {
-        return new Promise((resolve, reject) =>
-        {
+    connect(): Promise<Furti> {
+        return new Promise((resolve, reject) => {
             const uri = `mongodb://${this.username}:${this.password}@127.0.0.1:27017/furti`;
 
             MongoClient.connect(uri)
-                .then(db =>
-                {
+                .then(db => {
                     console.log('connected to mongo instance');
                     this.db = db;
 
                     resolve(this);
-                }).catch(reason =>
-                {
+                })
+                .catch(reason => {
                     console.log(`Error authenticating on database: ${reason}`);
                     reject(reason);
                 });
         });
     }
 
-    updateSection(section: any, options: UpdateOptions): Promise<any>
-    {
+    updateSection(section: any, options: UpdateOptions): Promise<any> {
         const sectionToUpdate = Object.assign({}, section);
         sectionToUpdate._id = sectionToUpdate.id;
         delete sectionToUpdate.id;
 
-        return this.findSectionById(sectionToUpdate._id).count()
-            .then(count =>
-            {
-                if (count === 0)
-                {
+        return this.findSectionById(sectionToUpdate._id)
+            .count()
+            .then(count => {
+                if (count === 0) {
                     console.log('Creating new section');
 
-                    return this.db.collection('sections')
+                    return this.db
+                        .collection('sections')
                         .insert(sectionToUpdate)
-                        .then(result =>
-                        {
+                        .then(result => {
                             console.log(`Added section`, result);
 
-                            if (options.autoclose)
-                            {
+                            if (options.autoclose) {
                                 return this.close();
                             }
 
                             return result;
                         }) as Promise<any>;
-                }
-                else
-                {
+                } else {
                     console.log('Updating existing section');
 
-                    return this.db.collection('sections')
-                        .updateOne({
-                            _id: sectionToUpdate._id
-                        }, {
-                            $set: sectionToUpdate
-                        }).then(result =>
-                        {
+                    return this.db
+                        .collection('sections')
+                        .updateOne(
+                            {
+                                _id: sectionToUpdate._id
+                            },
+                            {
+                                $set: sectionToUpdate
+                            }
+                        )
+                        .then(result => {
                             console.log(`Updated section ${result}`);
 
-                            if (options.autoclose)
-                            {
+                            if (options.autoclose) {
                                 return this.close();
                             }
 
@@ -82,35 +75,107 @@ export class Furti
                         }) as Promise<any>;
                 }
             })
-            .catch((reason: any) =>
-            {
+            .catch((reason: any) => {
                 console.error(`Error updating or inserting section ${reason}`);
 
-                if (options.autoclose)
-                {
+                if (options.autoclose) {
                     return this.close();
                 }
             });
     }
 
-    findSectionById(id: number): Cursor<any>
-    {
+    updateUser(user: any, options: UpdateOptions): Promise<any> {
+        const userToUpdate = Object.assign({}, user);
+        userToUpdate._id = userToUpdate.id;
+        delete userToUpdate.id;
+
+        return this.findUserById(userToUpdate._id)
+            .count()
+            .then(count => {
+                if (count === 0) {
+                    console.log('Creating new user');
+
+                    return this.db
+                        .collection('users')
+                        .insert(userToUpdate)
+                        .then(result => {
+                            console.log(`Added user`, result);
+
+                            if (options.autoclose) {
+                                return this.close();
+                            }
+
+                            return result;
+                        }) as Promise<any>;
+                } else {
+                    console.log('Updating existing user');
+
+                    return this.db
+                        .collection('users')
+                        .updateOne(
+                            {
+                                _id: userToUpdate._id
+                            },
+                            {
+                                $set: userToUpdate
+                            }
+                        )
+                        .then(result => {
+                            console.log(`Updated user ${result}`);
+
+                            if (options.autoclose) {
+                                return this.close();
+                            }
+
+                            return result;
+                        }) as Promise<any>;
+                }
+            })
+            .catch((reason: any) => {
+                console.error(`Error updating or inserting user ${reason}`);
+
+                if (options.autoclose) {
+                    return this.close();
+                }
+            });
+    }
+
+    updateUserPassword(username: string, password: string, options: UpdateOptions): Promise<any> {
+        return this.db
+            .collection('users')
+            .updateOne(
+                { username },
+                {
+                    $set: { password }
+                }
+            )
+            .then(() => {
+                if (options.autoclose) {
+                    return this.close();
+                }
+            });
+    }
+
+    findSectionById(id: number): Cursor<any> {
         return this.db.collection('sections').find({ _id: id });
     }
 
-    close(): void
-    {
-        this.db.close().then(() =>
-        {
-            console.log('Disconnected from database');
-        }).catch(reason =>
-        {
-            console.error(`Error while disconnecting ${reason}`);
-        });
+    findUserById(id: string): Cursor<any> {
+        return this.db.collection('users').find({ _id: id });
+    }
+
+    close(): void {
+        this.db
+            .close()
+            .then(() => {
+                console.log('Disconnected from database');
+            })
+            .catch(reason => {
+                console.error(`Error while disconnecting ${reason}`);
+            });
     }
 }
 
-export interface UpdateOptions
-{
+export interface UpdateOptions {
     autoclose: boolean;
 }
---
+++ database/scripts/users.ts
@@ -0,0 +1,117 @@
+import { ArgumentParser } from 'argparse';
+import * as fs from 'fs';
+import * as path from 'path';
+import { randomBytes } from 'crypto';
+const bcrypt: any = require('bcrypt');
+
+import { Furti } from './furti';
+
+const parser = new ArgumentParser({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'Create Users'
+});
+
+parser.addArgument(['--new'], {
+    help: 'Create a new user. node users.js --new <username>'
+});
+
+parser.addArgument(['--import'], {
+    help: 'Import the user into the database. node users.js --import <username>'
+});
+
+parser.addArgument(['--createpass'], {
+    help: 'Create a new password for the user. node users.js --createpass <username>'
+});
+
+parser.addArgument(['--user'], {
+    help: 'Username to connect to the database',
+    required: true
+});
+
+parser.addArgument(['--password'], {
+    help: 'Password to connect to the database',
+    required: true
+});
+
+const args = parser.parseArgs();
+
+function jsonFile(username: string) {
+    return path.join(process.cwd(), `database/import/users/${username}.json`);
+}
+
+function createId() {
+    const bytes = randomBytes(32);
+
+    return bytes.toString('base64');
+}
+
+function createPassword(): string {
+    const passwordBytes = randomBytes(8);
+    return passwordBytes.toString('base64');
+}
+
+function encodePassword(password: string): string {
+    return bcrypt.hashSync(password, 10);
+}
+
+if (args.new) {
+    const emptyUser = {
+        id: createId(),
+        username: args.new,
+        name: '',
+        roles: ['']
+    };
+
+    const userFile = jsonFile(args.new);
+
+    fs.exists(userFile, exists => {
+        if (exists) {
+            console.log('File already exists');
+        } else {
+            fs.writeFile(userFile, JSON.stringify(emptyUser, null, 4), 'utf-8', err => {
+                if (err) {
+                    throw err;
+                }
+
+                console.log(`Userfile ${userFile} written`);
+            });
+        }
+    });
+} else if (args.import) {
+    const userFile = jsonFile(args.import);
+
+    fs.readFile(userFile, 'utf-8', (err, content) => {
+        if (err) {
+            throw err;
+        }
+
+        const user = JSON.parse(content);
+
+        const furti = new Furti(args.user, args.password);
+
+        furti.connect().then(db => {
+            db.updateUser(user, {
+                autoclose: true
+            });
+        });
+    });
+} else if (args.createpass) {
+    const username = args.createpass;
+    const password = createPassword();
+    const encodedPassword = encodePassword(password);
+
+    const furti = new Furti(args.user, args.password);
+
+    furti.connect().then(db => {
+        db
+            .updateUserPassword(username, encodedPassword, {
+                autoclose: true
+            })
+            .then(() => {
+                console.log(`Set password ${password} for user ${username}`);
+            });
+    });
+} else {
+    console.log('No argument specified!');
+}
